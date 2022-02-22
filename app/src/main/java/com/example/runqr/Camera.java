d22cc16dd2bd2e0ae8385b632e7a604a39dc4497
KennyUA <kke@ualberta.ca>
2022-02-22 08:42:56 -0700

    started on Camera and Photo class

---
+++ app/src/main/java/com/example/runqr/Camera.java
@@ -0,0 +1,5 @@
+package com.example.runqr;
+
+public class Camera {
+    //impliment camera class from android studios "https://developer.android.com/guide/topics/media/camera"
+}
---
+++ app/src/main/java/com/example/runqr/Photo.java
@@ -0,0 +1,13 @@
+package com.example.runqr;
+
+public class Photo {
+    private byte image;
+
+    public byte getImage() {
+        return image;
+    }
+
+    public void setImage(byte image) {
+        this.image = image;
+    }
+}
