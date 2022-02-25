80bbe4ba603f20c726a54ebd0681b873c4d695f5
M1Al3x <kasych@ualberta.ca>
2022-02-25 02:12:49 -0600

    added Location and Map classes

--- app/src/main/java/com/example/runqr/QRCode.java
+++ app/src/main/java/com/example/runqr/QRCode.java
@@ -6,9 +6,23 @@ public class QRCode {
 
     private String hash;
 
+    private Location location;
 
+    public QRCode(int score, String hash, Location location) {
+        this.score = score;
+        this.hash = hash;
+        this.location = location;
+    }
 
+    public int getScore() {
+        return score;
+    }
 
+    public String getHash() {
+        return hash;
+    }
 
-
+    public Location getLocation() {
+        return location;
+    }
 }
