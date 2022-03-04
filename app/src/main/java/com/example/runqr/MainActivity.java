4a65a3f6ec48753af4a856c767a4ab8b63aff8f4
KennyUA <kke@ualberta.ca>
2022-03-03 19:33:46 -0700

    started database

--- app/src/main/java/com/example/runqr/MainActivity.java
+++ app/src/main/java/com/example/runqr/MainActivity.java
@@ -20,6 +20,7 @@ import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
@@ -30,6 +31,10 @@ public class MainActivity extends AppCompatActivity implements AddQRFragment.OnC
     /// fix below to do automatic log in and save player info
     Player currentPlayer = new Player();
 
+    // Access a Cloud Firestore instance from your Activity
+    FirebaseFirestore db;
+
+    db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
