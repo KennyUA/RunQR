e053750c23e9de293505fd124f8d1d15974bdb00
AYUSH ROY <ayush3@ualberta.ca>
2022-03-05 01:54:17 -0700

    Added map stuff in Main Activity

--- app/src/main/java/com/example/runqr/MainActivity.java
+++ app/src/main/java/com/example/runqr/MainActivity.java
@@ -33,6 +33,8 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+import com.mapbox.maps.MapView;
+import com.mapbox.maps.Style;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -45,6 +47,7 @@ public class MainActivity extends AppCompatActivity implements AddQRFragment.OnC
 
     // Access a Cloud Firestore instance from your Activity
     FirebaseFirestore db;
+    MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -84,6 +87,11 @@ public class MainActivity extends AppCompatActivity implements AddQRFragment.OnC
 
 
          */
+
+        //Map Stuff
+
+        mapView = findViewById(R.id.map);
+        mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS);
 
         // The set method sets a unique id for the document
         HashMap<String, String> accountData = new HashMap<>();
@@ -125,6 +133,26 @@ public class MainActivity extends AppCompatActivity implements AddQRFragment.OnC
 
     }
 
+    public void onStart(){
+        super.onStart();
+        mapView.onStart();
+    }
+
+    public void onStop(){
+        super.onStop();
+        mapView.onStop();
+    }
+
+    public void onLowMemory(){
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    public void onDestroy(){
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
 
     public void openAddQRFragment(Button addQR){
         // open addQRFragment to scan QRcode and add it to player's account
@@ -153,6 +181,7 @@ public class MainActivity extends AppCompatActivity implements AddQRFragment.OnC
         //transaction.commit();
 
     }
+
 
 
     @Override
