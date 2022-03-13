77dbea8576c90026ff1b7343d25d525b05f5a9c7
Yumna Arshad <yumnaverified@gmail.com>
2022-03-12 18:42:21 -0700

    changed documentation for newly made Hasher class

--- app/src/main/java/com/example/runqr/Hasher.java
+++ app/src/main/java/com/example/runqr/Hasher.java
@@ -6,22 +6,17 @@ import java.io.Serializable;
 import java.nio.charset.StandardCharsets;
 
 /**
- * This class represents the scanner used by player to scan QRCode objects in the RunQR game.
- * This class is responsible for both scanning the QRCode and hashing its string contents so that contents are not stored by the game (for privacy concerns).
- * Once hashed, Scanner creates a new QRCode object with the resulting hash.
- * Scanner is used so that the String contents of QRCode are not stored or known by the QRCode object.
+ * This class represents the hasher which takes in contents of QRCode scanned in AddQRFragment and hashes them.
+ * This class is responsible for hashing scanned QRCodes's string contents so that contents are not stored by the game (for privacy concerns).
+ * Hasher is used as an intermediary so that contents of the QRCode are not stored or known by the QRCode object.
  */
 public class Hasher implements Serializable {
-
-    private String hash;
-
 
     public String hashQRCode(String rawContent){
 
         String sha256hex = Hashing.sha256()
                 .hashString(rawContent, StandardCharsets.UTF_8)
                 .toString();
-        //this.hash = sha256hex;
         return sha256hex;
     }
 
