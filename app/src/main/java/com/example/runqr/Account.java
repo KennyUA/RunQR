49c0cce63fa7a37f507d70f5c2512593202fbc1e
M1Al3x <90932420+M1Al3x@users.noreply.github.com>
2022-03-14 04:30:18 -0600

    Fixed bag with null player.

--- app/src/main/java/com/example/runqr/Account.java
+++ app/src/main/java/com/example/runqr/Account.java
@@ -1,8 +1,14 @@
 package com.example.runqr;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 
+/**
+ * This class represents a player's account in the game.
+ * Account has 2 main attributes: username, and contactEmail:
+ * A username is chosen by player and is used to uniquely identify the player in the game.
+ * A contactEmail is given by player and is used to allow for communication with player.
+ *
+ */
 public class Account  implements Serializable {
 
     private String username;
@@ -17,14 +23,30 @@ public class Account  implements Serializable {
         this.contactEmail = contactEmail;
     }
 
+<<<<<<< HEAD
+=======
     //public ArrayList<>
 
 
+>>>>>>> ceedd5f6b56cb4f3360a61c496759026fb77a3a7
 
+    /**
+     * This method gets the username associated with a given player's account.
+     * @return
+     *      String representing the account's username.
+     */
     public String getUsername(){
         return this.username;
     }
 
+    /**
+     * This method gets the contact information (email) associated with a given player's account.
+     * @return
+     *      String representing the account's email.
+     */
+
+
+    public String getContactEmail() { return this.contactEmail; }
 
 
 
