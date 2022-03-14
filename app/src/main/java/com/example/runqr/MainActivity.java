dceefc000a2f5afe8e37d015dc0868963fe10162
AYUSH ROY <ayush3@ualberta.ca>
2022-03-13 20:18:14 -0600

    Added fragment to display actual pin locations on button click

--- app/src/main/java/com/example/runqr/MainActivity.java
+++ app/src/main/java/com/example/runqr/MainActivity.java
@@ -72,6 +72,7 @@ public class MainActivity extends AppCompatActivity implements AddQRFragment.OnF
     FloatingActionButton loadBtn;
     FloatingActionButton listBtn;
     ArrayList<Marker> markerArrayList;
+    ArrayList<MarkerOptions> markerOptionsArrayList;
     GoogleMap currentMap;
     //cite https://stackoverflow.com/questions/48699032/how-to-set-addsnapshotlistener-and-remove-in-populateviewholder-in-recyclerview
     EventListener<QuerySnapshot> eventListener;
@@ -129,8 +130,8 @@ public class MainActivity extends AppCompatActivity implements AddQRFragment.OnF
 
          */
         /*QRCodesReference = db.collection("QR Codes");
-        qrData.put("Location X", "53.5627");
-        qrData.put("Location Y", "-113.5055");
+        qrData.put("Location X", "53.5198");
+        qrData.put("Location Y", "-113.4970");
         int random_int = (int)Math.floor(Math.random()*(100-0+1)+0);
 
         QRCodesReference
@@ -151,7 +152,7 @@ public class MainActivity extends AppCompatActivity implements AddQRFragment.OnF
                         Log.v(TAG, "Global QRData could not be added!" + e.toString());
                     }
                 });*/
-
+        markerOptionsArrayList = new ArrayList<MarkerOptions>();
         markerArrayList = new ArrayList<Marker>();
         QRCodesReference.addSnapshotListener(new EventListener<QuerySnapshot>() {
             @Override
@@ -161,8 +162,10 @@ public class MainActivity extends AppCompatActivity implements AddQRFragment.OnF
                        marker.remove();
                    }}
 
-               markerArrayList.clear();
 
+
+               markerArrayList.clear();
+               markerOptionsArrayList.clear();
 
                 for(QueryDocumentSnapshot doc: queryDocumentSnapshots){
                     if((String) doc.getId() != "unique hash"){
@@ -171,10 +174,12 @@ public class MainActivity extends AppCompatActivity implements AddQRFragment.OnF
                         Log.v("y", String.valueOf(doc.getData().get("Location Y")));
                         Float x = Float.parseFloat((String)doc.getData().get("Location X"));
                         Float y = Float.parseFloat((String)doc.getData().get("Location Y"));
-                        Marker newMarker = currentMap.addMarker(new MarkerOptions()
+                        MarkerOptions newMarkerOptions = new MarkerOptions()
                                 .position(new LatLng(x, y))
-                                .title("new Marker"));
+                                .title("new Marker");
+                        Marker newMarker = currentMap.addMarker(newMarkerOptions);
 
+                        markerOptionsArrayList.add(newMarkerOptions);
                         markerArrayList.add(newMarker);
                     }
 
@@ -189,7 +194,14 @@ public class MainActivity extends AppCompatActivity implements AddQRFragment.OnF
 
         listBtn = findViewById(R.id.searchLocationsBtn);
 
+        listBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MarkerFragment locationFragment = MarkerFragment.newInstance(markerOptionsArrayList);
+                locationFragment.show(getSupportFragmentManager(), "LOCATIONS");
 
+            }
+        });
 
 
 
---
+++ app/src/main/java/com/example/runqr/MarkerFragment.java
@@ -0,0 +1,78 @@
+package com.example.runqr;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import com.example.runqr.placeholder.PlaceholderContent;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+
+/**
+ * A fragment representing a list of Items.
+ */
+public class MarkerFragment extends DialogFragment {
+
+
+    private ListView locationList;
+    ArrayAdapter<MarkerOptions> locationsAdapter;
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public MarkerFragment() {
+    }
+
+    // TODO: Customize parameter initialization
+    @SuppressWarnings("unused")
+    public static MarkerFragment newInstance(ArrayList<MarkerOptions> MarkerArray) {
+        MarkerFragment fragment = new MarkerFragment();
+        Bundle args = new Bundle();
+        args.putParcelableArrayList("markerArray", MarkerArray);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public void onAttach(Context context){
+        super.onAttach(context);
+
+    }
+
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.list_fragment, null);
+        locationList = view.findViewById(R.id.qrListView);
+
+        //cite https://camposha.info/android-examples/android-list-fragment/#gsc.tab=0
+        ArrayList<MarkerOptions> locationsList = getArguments().getParcelableArrayList("markerArray");
+
+        locationsAdapter = new CustomListAdapter(getContext(), locationsList);
+        locationList.setAdapter(locationsAdapter);
+
+        AlertDialog.Builder  builder = new AlertDialog.Builder((getContext()));
+        return builder
+                .setView(view)
+                .setNegativeButton("Cancel", null)
+                .setPositiveButton("Ok", null).create();
+
+    }
+}
