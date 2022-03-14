5e3f2854657adcfba505a7457bad6253e5d0e9e6
Yumna Arshad <yumnaverified@gmail.com>
2022-03-14 15:33:02 -0600

    added Oleksandr's test from my branch bc his android studio was failing

--- app/src/main/java/com/example/runqr/Account.java
+++ app/src/main/java/com/example/runqr/Account.java
@@ -38,8 +38,6 @@ public class Account  implements Serializable {
      * @return
      *      String representing the account's email.
      */
-
-
     public String getContactEmail() { return this.contactEmail; }
 
 
