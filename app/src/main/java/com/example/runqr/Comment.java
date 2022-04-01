b14171d8b99e8135a26e5ffdaa34c4715e767bfd
Yumna Arshad <yumnaverified@gmail.com>
2022-03-31 23:01:58 -0600

    reverted to previous version and started working on comments for qrCodes

---
+++ app/src/main/java/com/example/runqr/Comment.java
@@ -0,0 +1,27 @@
+package com.example.runqr;
+
+public class Comment {
+    String title;
+    String body;
+
+    public Comment(String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
--- app/src/main/java/com/example/runqr/DisplayQRCodeActivity.java
+++ app/src/main/java/com/example/runqr/DisplayQRCodeActivity.java
@@ -1,17 +1,29 @@
 package com.example.runqr;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 // This class is the activity that displays the details of QRCodes and allows player to browse their QRCodes.
 // Once an item, corresponding to a QRCode, in ListView of QRLibraryActivity is clicked the DisplayQRActivity is opened to display QRCode details.
 // QRCode details being displayed so far are: score, location (X and Y coordinates) if it exists, and image if it exists.
 
 
-public class DisplayQRCodeActivity extends AppCompatActivity {
+public class DisplayQRCodeActivity extends AppCompatActivity implements AddCommentFragment.OnFragmentInteractionListener {
+
+    //for editing comments
+    Boolean edit = false;
+    Integer clickPos;
+    Comment clickedComment = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +35,17 @@ public class DisplayQRCodeActivity extends AppCompatActivity {
         ImageView QRCodePhoto = findViewById(R.id.photoInfo);
         TextView QRCodeStats = findViewById(R.id.statInfo);
         TextView QRCodeLocation = findViewById(R.id.locationInfo);
+        RecyclerView QRCodeComments = findViewById(R.id.recyclerView);
+        ArrayList<Comment> commentDataList = codeToDisplay.getCommentLibrary().getCommentList();
+
+        // Setting the layout as linear
+        // layout for vertical orientation
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+        QRCodeComments.setLayoutManager(linearLayoutManager);
+
+        // Sending reference and data to Adapter
+        CommentList commentAdapter = new CommentList(DisplayQRCodeActivity.this, codeToDisplay.getCommentLibrary().getCommentList());
+        QRCodeComments.setAdapter(commentAdapter);
 
         QRCodeLocation.setText("Location: \n");
         if (codeToDisplay.getLocation() != null){
@@ -34,6 +57,39 @@ public class DisplayQRCodeActivity extends AppCompatActivity {
 
         }
         QRCodeStats.setText("Score: "+codeToDisplay.getScore());
+
+        /*
+        //add on item click listener for recycler view
+
+        QRCodeComments.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                edit = true;
+                clickPos = position;
+                clickedComment = (Comment) cityList.getItemAtPosition(position);
+                new AddCityFragment().show(getSupportFragmentManager(), "EDIT_CITY");
+
+
+            }
+        });
+
+         */
+    }
+
+    @Override
+    public void onOkPressed(Comment newComment) {
+        if (edit) {
+            //cDataList.remove(clickPos);
+            //cityList.setAdapter(cityAdapter);
+            //commentAdapter.remove(clickedComment);
+            cityAdapter.add(newCity);
+            //cityList.setAdapter(cityAdapter);
+        }
+        else {
+            cityAdapter.add(newCity);
+        }
+        edit = false;
 
     }
 }
