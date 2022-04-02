09f185d4f6517d12fa667adaa108c4988af611a9
KennyUA <kke@ualberta.ca>
2022-04-02 12:10:38 -0600

    code up to date

--- app/src/main/java/com/example/runqr/Camera.java
+++ app/src/main/java/com/example/runqr/Camera.java
@@ -1,27 +1,152 @@
 package com.example.runqr;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
 import android.hardware.camera2.CameraManager;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.io.FileNotFoundException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
 
 
-public class Camera {
+public class Camera extends AppCompatActivity implements View.OnClickListener {
     //impliment camera class from android studios "https://developer.android.com/guide/topics/media/camera"
-    private boolean checkCamera(Context context){
-        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
-            return true;
-        } else{
-            return false;
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+
+    PreviewView previewView;
+    Button takePicture;
+    private ImageCapture imageCapture;
+    Bitmap bitmap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        takePicture = findViewById(R.id.takePhoto);
+
+        previewView = findViewById(R.id.previewView);
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                startCameraX(cameraProvider);
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, getExecutor());
+    }
+
+    private Executor getExecutor() {
+        return ContextCompat.getMainExecutor(this);
+    }
+
+    private void startCameraX(ProcessCameraProvider cameraProvider) {
+        cameraProvider.unbindAll();
+
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+
+        Preview preview = new Preview.Builder().build();
+
+        preview.setSurfaceProvider(previewView.createSurfaceProvider());
+
+        imageCapture = new ImageCapture.Builder()
+                .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+                .build();
+
+        cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture);
+    }
+
+    @Override
+    public void onClick(View view){
+        if (view.getId() == R.id.takePhoto) {
+            capturePhoto();
         }
     }
 
-    public static Camera getCameraInstance(){
-        Camera c = null;
-        try {
-            //c =
-        }
-        catch (Exception e){
-        }
-        return c;
+    private void capturePhoto() {
+
+        //File photoDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/CameraXPhotos");
+        //if(!photoDir.exists())
+        //photoDir.mkdir();
+
+
+
+        //Date date = new Date();
+        //String timestamp = String.valueOf(date.getTime());
+        long timestamp = System.currentTimeMillis();
+        //String photoFilePath = photoDir.getAbsolutePath() + "/" + timestamp + ".jpg";
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, timestamp);
+        contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg");
+
+        //File photoFile = new File(photoFilePath);
+
+        //File photoFile;
+        //photoFile = new File(Environment.getExternalStorageDirectory(),
+        //"IMG_${System.currentTimeMillis()}.jpg");
+
+        // Create output options object which contains file + metadata
+        //ImageCapture.OutputFileOptions outputOptions = new ImageCapture.OutputFileOptions.Builder(photoFile).build();
+
+        imageCapture.takePicture(
+
+                new ImageCapture.OutputFileOptions.Builder(
+                        getContentResolver(),
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                        contentValues
+                ).build(),
+                getExecutor(),
+                new ImageCapture.OnImageSavedCallback(){
+
+                    @Override
+                    public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
+                        //ImageDecoder.Source source = ImageDecoder.createSource(contentValues, outputFileResults.getSavedUri());
+                        Context context = null;
+                        try {
+                            context.getContentResolver().openInputStream(outputFileResults.getSavedUri());
+
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                        Log.v("hello","Photo saved");
+                    }
+
+                    @Override
+                    public void onError(@NonNull ImageCaptureException exception) {
+                        Log.v("hello","Photo not saved");
+
+                    }
+                }
+
+        );
+
     }
+
 }
