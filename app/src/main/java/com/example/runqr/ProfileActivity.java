da4d5f7dce1887ebcf71f9f89fb0f2131579b9ca
Eluviian <bayleysapara@gmail.com>
2022-04-04 13:13:17 -0600

    whenever profile activity is opened, rankings get reverted to N/A

--- app/src/main/java/com/example/runqr/ProfileActivity.java
+++ app/src/main/java/com/example/runqr/ProfileActivity.java
@@ -1,15 +1,23 @@
 package com.example.runqr;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 
@@ -27,7 +35,9 @@ public class ProfileActivity extends AppCompatActivity {
     ArrayList<ProfileItem> profileDataList;
     String[] items;
     String[] values;
-
+    private String rankNumOfScannedString;
+    private String rankHighQrString;
+    private String rankSumOfScoresString;
 
 
     @Override
@@ -46,9 +56,9 @@ public class ProfileActivity extends AppCompatActivity {
 
         String highQrString = "N/A";
         String lowQrString = "N/A";
-        String rankHighQrString = "N/A";
-        String rankNumOfScannedString = "N/A";
-        String rankSumOfScoresString = "N/A";
+        //String rankHighQrString = "N/A";
+        //String rankNumOfScannedString = "N/A";
+        //String rankSumOfScoresString = "N/A";
 
 
         if (playerStats.getHighQr() != null) {
@@ -59,7 +69,7 @@ public class ProfileActivity extends AppCompatActivity {
             lowQrString = String.valueOf(playerStats.getLowQr().getScore());
         }
 
-        /*
+
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         db.collection("Accounts").document(playerStats.username).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
@@ -68,9 +78,9 @@ public class ProfileActivity extends AppCompatActivity {
                     DocumentSnapshot document = task.getResult();
                     if (document.exists()) {
                         Log.d(TAG, "entered firebase profile activity");
-                        String rankHighQrString = (String) document.get("playerInfo.playerStats.rankHighQr");
-                        String rankNumOfScannedString = (String) document.get("playerInfo.playerStats.rankNumOfScanned");
-                        String rankSumOfScoresString = (String) document.get("playerInfo.playerStats.rankSumOfScores");
+                        String rankHighQrString = (String) document.get("playerStats.rankHighQr");
+                        String rankNumOfScannedString = (String) document.get("playerStats.rankNumOfScanned");
+                        String rankSumOfScoresString = (String) document.get("playerStats.rankSumOfScores");
                     }
                 } else {
                     Log.d(TAG, "Error getting documents: ", task.getException());
@@ -78,7 +88,7 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
-         */
+
 
         String[] items = {"Scanned QR Codes: ", "Total Score: ", "Rank (number of codes): ", "Rank (player): ", "Rank (highest scoring code): ", "Highest Scoring: ", "Lowest Scoring: "};
         String[] values = {String.valueOf(playerStats.getNumOfScanned()), String.valueOf(playerStats.getSumOfScores()), rankNumOfScannedString, rankSumOfScoresString, rankHighQrString, highQrString, lowQrString};
